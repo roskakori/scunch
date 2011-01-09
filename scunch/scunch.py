4d91672b71bfd8174e60e86f8a02cbab72c37dee
Thomas Aglassinger <roskakori@users.sourceforge.net>
2011-01-09 13:13:17 +0100

    Cleaned up copyright comment, which at one point inconsistently was referring to the GPL instead of LGPL.

--- scunch/scunch.py
+++ scunch/scunch.py
@@ -455,8 +455,8 @@ option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-more details.
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
@@ -514,8 +514,8 @@ Added options to normalize text files and fixed some critical bugs.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
