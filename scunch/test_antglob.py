c36208ce5f816b2ac3fca6cec4328d5d0bdf68c6
Thomas Aglassinger <roskakori@users.sourceforge.net>
2011-02-23 21:43:25 +0100

    Cleaned up unused import.

--- scunch/test_antglob.py
+++ scunch/test_antglob.py
@@ -15,7 +15,6 @@ Tests for antglob.
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import doctest
 import errno
 import logging
 import os
